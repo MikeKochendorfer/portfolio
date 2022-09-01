e07935c56e18a53af5817ad7592256893ad3942b
MikeKochendorfer <mike.kochendorfer@gmail.com>
2022-09-01 14:18:36 -0600

    style and place content on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import IntroCard from "../components/IntroCard";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>Portfolio Site</div>;
+  return (
+    <>
+      <Head>
+        <title>Michael Kochendorfer | Portfolio</title>
+      </Head>
+      <main className={styles.main}>
+        <IntroCard />
+      </main>
+    </>
+  );
 };
 
 export default Home;
