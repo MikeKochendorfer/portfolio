e847eef3f275e5e727e7291775c4f34257d56832
MikeKochendorfer <mike.kochendorfer@gmail.com>
2022-11-01 16:10:29 -0700

    update: about page, add: about page styles

--- components/About.tsx
+++ components/About.tsx
@@ -1,53 +1,60 @@
 import Avatar from "./Avatar";
 import Link from "next/link";
+import styles from "../styles/AboutStyles.module.css";
+import Button from "./Button";
+import Router from "next/router";
 
 export default function About(): JSX.Element {
   return (
-    <main>
+    <main className={styles.main}>
       <h1>About Me</h1>
       <Avatar
-        imageSrc="/portfolio-picture.jpg"
+        imageSrc="/about-me.jpg"
         hasAnimatedBorder={false}
         altText="Picture of Michael Kochendorfer"
       />
-      <p>
-        I was born and raised in the southwestern suburbs of Chicago; the windy
-        city, with the best pizza and hotdogs you can find. In my adulthood, I
-        moved to Los Angeles to pursue my Master&apos;s Degree and it was there
-        that I encountered the love of my life and married.
-      </p>
-      <br />
-      <p>
-        My entry into the world of programming was somewhat of a happy mistake.
-        My studies were primarily concerned with theoretical, historical,
-        philosophical, and abstract ideas; things which I certainly enjoy, but
-        which also left me yearning for engagement with more objective,
-        practical, or perhaps even mechanical experiences. At that time, to try
-        and provide some balance in my life, I started searching for a hobby
-        that tended towards a more concrete, either it works or it doesn&apos;t,
-        manner of thinking; something that would still be intellectually
-        stimulating and enjoyable, but in an entirely different way. So, in 2019
-        I picked up coding as a hobby. I had always enjoyed technology and
-        coding just seemed...well, cool.
-      </p>
-      <br />
-      <p>
-        What was initially just a hobby quickly turned into a career interest.
-        Coding fulfills my deep sense of curiosity and desire to always be
-        learning. At times, it almost feels like magic, or a having a
-        superpower; in others, (when tracking down an elusive bug, for example)
-        it is incredibly frustrating and yet, even still, so rewarding; always
-        providing me with a challenge to overcome, a problem to be solved.
-      </p>
-      <br />
-      <p>
-        I am currently freelancing and finding value in providing quality,
-        custom websites for my clients. If you are in need of a website or your
-        company is hiring developers, please don&apos;t hesitate to{" "}
-        <Link href={"/contact"} passHref>
-          <a>get in touch.</a>
-        </Link>
-      </p>
+      <div className={styles.textContent}>
+        <p>
+          Hi, I&apos;m Mike 👋. I provide custom web development with a focus on
+          modern technologies like React, Next.js, and GraphQL. I like to keep
+          up with the latest trends in technology and consider myself a
+          life-long learner. I find value in helping others accomplish their
+          goals. I am a listener and an achiever; I would love to hear what you
+          are trying to accomplish. If you need a website or your company is
+          hiring developers, please don&apos;t hesitate to{" "}
+          <Link href={"/contact"} passHref>
+            <a>get in touch.</a>
+          </Link>{" "}
+          If you would like to get to know me a bit more first, keep
+          reading...👀
+        </p>
+        <br />
+        <p>
+          I grew up in the suburbs of Chicago, moved to Los Angeles to pursue a
+          Master&apos;s degree, got married, and have spent the last few years
+          in Chihuahua, Mexico with my wife who was born here. I value diversity
+          and believe that difference in unity can bring about beautiful,
+          useful, and equitable things in our world. I love to have new
+          experiences whether it be trying a new restaurant, visiting a new
+          city, getting a great music recommendation, or reading/watching an
+          exciting, new mind-bending series.
+        </p>
+        <br />
+        <p>
+          I&apos;m a bit of a nerd 🖖. I tend to be introspective and
+          introverted, but I can also talk for hours about the things I enjoy.
+          In my free time, you can find me playing guitar, walking out in
+          nature, or playing board games with some good friends.
+        </p>
+        <br />
+        <p>Enough about me...Let&apos;s build something great together 🚀</p>
+        <br />
+        <Button
+          variant={"primary"}
+          buttonText={"Contact Me"}
+          onClick={() => Router.push("/contact")}
+        />
+      </div>
     </main>
   );
 }
