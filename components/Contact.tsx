4e3720e97f51d24f2103bc83024cdcf44ed0e909
MikeKochendorfer <mike.kochendorfer@gmail.com>
2022-11-18 20:01:57 -0600

    feature: keep track of the form's state

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,60 +1,74 @@
+import React, { useState } from "react";
 import styles from "../styles/ContactStyles.module.css";
 import Button from "./Button";
 
 export default function Contact(): JSX.Element {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    purpose: "",
+    description: ""
+  })
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    let { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value })
+  }
+  
   return (
     <div className={styles.contactWrapper}>
       <h1>Contact</h1>
       {/* <p>I would love to discuss the project or role you are looking to </p> */}
       <form method="post">
         <label htmlFor="name">Name/Company Name:</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleInput} />
 
         <label htmlFor="email">Email:</label>
-        <input type="email" name="email" />
+        <input type="email" name="email" onChange={handleInput} />
 
         <fieldset>
           <legend>How can I help you?</legend>
 
           <div className={styles.radioGroup}>
             <div>
-              <input type="radio" name="contact" value="I need a website" />
-              <label htmlFor="contact">I need a website</label>
+              <input type="radio" name="purpose" value="I need a website" onChange={handleInput} />
+              <label htmlFor="purpose">I need a website</label>
             </div>
 
             <div>
               <input
                 type="radio"
-                name="contact"
+                name="purpose"
                 value="My company is (or I am) hiring a developer"
+                onChange={handleInput}
               />
-              <label htmlFor="contact">
+              <label htmlFor="purpose">
                 My company is (or I am) hiring developers
               </label>
             </div>
 
             <div>
-              <input type="radio" name="contact" value="I am a designer" />
-              <label htmlFor="contact">
+              <input type="radio" name="purpose" value="I am a designer" onChange={handleInput}/>
+              <label htmlFor="purpose">
                 I am a designer looking to collaborate
               </label>
             </div>
 
             <div>
-              <input type="radio" name="contact" value="Other" />
-              <label htmlFor="contact">Other (please explain below)</label>
+              <input type="radio" name="purpose" value="Other" onChange={handleInput}/>
+              <label htmlFor="purpose">Other (please explain below)</label>
             </div>
           </div>
         </fieldset>
 
         <label htmlFor="description">Project or Role Description:</label>
-        <textarea name="description" cols={30} rows={10}></textarea>
+        <textarea name="description" cols={30} rows={10} onChange={handleInput}></textarea>
         <Button
           buttonText={"Submit Form"}
           variant={"primary"}
           onClick={(e) => {
             e.preventDefault();
-            console.log("form submitted");
+            console.log(inputs);
           }}
         />
       </form>
