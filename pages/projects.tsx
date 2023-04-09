15fe89c213d3a4f740ae62b7b504b02e432a6c45
MikeKochendorfer <mike.kochendorfer@gmail.com>
2023-04-09 14:57:12 -0600

    update:style: changes from a list to grid layout on projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -22,64 +22,66 @@ export default function ProjectsPage(): JSX.Element {
         />
       </Head>
       <h1>Projects</h1>
-      <ProjectCard
-        title={"Lucky Shrub"}
-        description={
-          "A responsive landing page for a fictional landscaping company."
-        }
-        imgURL={
-          "https://res.cloudinary.com/dfkgccmba/image/upload/v1669317789/portfolio/lucky-shrub_ddwaev.png"
-        }
-        keyTech={"HTML, CSS"}
-        repo={"https://github.com/MikeKochendorfer/lucky-shrub"}
-        url={"https://mikekochendorfer.github.io/lucky-shrub/"}
-      />
-      <ProjectCard
-        title={"QR Code Generator"}
-        description={
-          "A simple tool for generating a QR code which can be downloaded and used."
-        }
-        imgURL={
-          "https://res.cloudinary.com/dfkgccmba/image/upload/v1671057014/portfolio/qr-code-generator_wm4ve2.png"
-        }
-        keyTech={"TypeScript, Next.js"}
-        repo={"https://github.com/MikeKochendorfer/qr-code-generator"}
-        url={"qr-code-generator-mike-k.vercel.app"}
-      />
-      <ProjectCard
-        title={"Ver.De.Amor"}
-        description={
-          "A full-stack Ecommerce site with an administrative CMS for product and shipping management."
-        }
-        imgURL={
-          "https://res.cloudinary.com/dfkgccmba/image/upload/v1669317802/portfolio/verdeamor_cmgwhd.png"
-        }
-        keyTech={"Next.js, GraphQL, Keystone.js, Stripe, PostgreSQL"}
-      />
-      <ProjectCard
-        title={"Wine Pairing App"}
-        description={
-          "A single page application for finding a good style of wine to pair with the food of your choosing."
-        }
-        imgURL={
-          "https://res.cloudinary.com/dfkgccmba/image/upload/v1669317853/portfolio/wine-pairing_fyfbq2.png"
-        }
-        keyTech={"Next.js, Serverless functions, Third-Party API"}
-        repo={"https://github.com/MikeKochendorfer/wine-pairing-app"}
-        url={"https://wine-pairing-app-qga9.vercel.app/"}
-      />
-      <ProjectCard
-        title={"SICK FITS"}
-        description={
-          "A fully featured Ecommerce site with user authentication, product management, pagination, and much more."
-        }
-        imgURL={
-          "https://res.cloudinary.com/dfkgccmba/image/upload/v1669317813/portfolio/sickfits_p4zdah.png"
-        }
-        keyTech={
-          "Next.js, Apollo GraphQL, Styled Components, Jest, Node.js, TypeScript, MongoDB"
-        }
-      />
+      <div className={styles.grid}>
+        <ProjectCard
+          title={"Lucky Shrub"}
+          description={
+            "A responsive landing page for a fictional landscaping company."
+          }
+          imgURL={
+            "https://res.cloudinary.com/dfkgccmba/image/upload/v1669317789/portfolio/lucky-shrub_ddwaev.png"
+          }
+          keyTech={"HTML, CSS"}
+          repo={"https://github.com/MikeKochendorfer/lucky-shrub"}
+          url={"https://mikekochendorfer.github.io/lucky-shrub/"}
+        />
+        <ProjectCard
+          title={"QR Code Generator"}
+          description={
+            "A simple tool for generating a QR code which can be downloaded and used."
+          }
+          imgURL={
+            "https://res.cloudinary.com/dfkgccmba/image/upload/v1671057014/portfolio/qr-code-generator_wm4ve2.png"
+          }
+          keyTech={"TypeScript, Next.js"}
+          repo={"https://github.com/MikeKochendorfer/qr-code-generator"}
+          url={"qr-code-generator-mike-k.vercel.app"}
+        />
+        <ProjectCard
+          title={"Ver.De.Amor"}
+          description={
+            "A full-stack Ecommerce site with an administrative CMS for product and shipping management."
+          }
+          imgURL={
+            "https://res.cloudinary.com/dfkgccmba/image/upload/v1669317802/portfolio/verdeamor_cmgwhd.png"
+          }
+          keyTech={"Next.js, GraphQL, Keystone.js, Stripe, PostgreSQL"}
+        />
+        <ProjectCard
+          title={"Wine Pairing App"}
+          description={
+            "A single page application for finding a good style of wine to pair with the food of your choosing."
+          }
+          imgURL={
+            "https://res.cloudinary.com/dfkgccmba/image/upload/v1669317853/portfolio/wine-pairing_fyfbq2.png"
+          }
+          keyTech={"Next.js, Serverless functions, Third-Party API"}
+          repo={"https://github.com/MikeKochendorfer/wine-pairing-app"}
+          url={"https://wine-pairing-app-qga9.vercel.app/"}
+        />
+        <ProjectCard
+          title={"SICK FITS"}
+          description={
+            "A fully featured Ecommerce site with user authentication, product management, pagination, and much more."
+          }
+          imgURL={
+            "https://res.cloudinary.com/dfkgccmba/image/upload/v1669317813/portfolio/sickfits_p4zdah.png"
+          }
+          keyTech={
+            "Next.js, Apollo GraphQL, Styled Components, Jest, Node.js, TypeScript, MongoDB"
+          }
+        />
+      </div>
     </div>
   );
 }
